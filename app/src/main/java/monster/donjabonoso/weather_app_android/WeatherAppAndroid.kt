76068b221f7e73b4825a52b61d7eb2bf0a63121c
yaser <yaser.hip@gmail.com>
2023-11-30 20:14:55 -0700

    init

---
+++ app/src/main/java/monster/donjabonoso/weather_app_android/WeatherAppAndroid.kt
@@ -0,0 +1,7 @@
+package monster.donjabonoso.weather_app_android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherAppAndroid: Application()
