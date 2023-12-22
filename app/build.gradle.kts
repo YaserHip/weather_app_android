14c6db656e3783001dc8b612b4d2fec765a29390
yaser <yaser.hip@gmail.com>
2023-12-21 23:15:58 -0700

    test repositoryWeather added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "18"
     }
     buildFeatures {
         compose = true
@@ -63,6 +63,7 @@ dependencies {
 
     //hilt
     implementation("com.google.dagger:hilt-android:2.48")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
---
+++ app/src/test/java/monster/donjabonoso/weather_app_android/data/repository/ImplRepositoryWeatherTest.kt
@@ -0,0 +1,44 @@
+package monster.donjabonoso.weather_app_android.data.repository
+
+import assertk.assertThat
+import kotlinx.coroutines.runBlocking
+import monster.donjabonoso.weather_app_android.data.remote.Api
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
+class ImplRepositoryWeatherTest {
+
+    private lateinit var repositoryWeather: ImplRepositoryWeather
+    private lateinit var api: Api
+    private lateinit var mockWebServer: MockWebServer
+
+    @BeforeEach
+    fun setUp() {
+        mockWebServer = MockWebServer()
+        api = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(mockWebServer.url("/"))
+            .build()
+            .create()
+        repositoryWeather = ImplRepositoryWeather(api)
+    }
+
+    @Test
+    fun `Error response 404`() = runBlocking {
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(404)
+        )
+
+        val response = repositoryWeather.getData(123.0,123.0)
+
+        assertThat(response.data).equals(null)
+    }
+
+
+}
